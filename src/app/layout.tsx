72b1cb3bdae5779ddab89e3a7beea94434b77387
Elchin-Novruzov <88887189+Elchin-Novruzov@users.noreply.github.com>
2024-06-09 04:20:29 +0200

    Fix the Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/images/programming.png" />
+        <link rel="icon" href="/images/programming.png" type="image/x-icon">
+        <link rel="shortcut icon" href="/images/programming.png" type="image/x-icon"></link>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
         <meta name="description" content="Elchin Novruzov personal website. This is a place where you can find my projects and information about me." />
         <meta name="keywords" content="Elchin Novruzov, Elchin (Elçin) Novruzov, Elçin Novruzov, Elcin Novruzov, Web Developer, Front-end developer, Html, Css, Js, Javascript, React, React.js, Next js, Node, Node.js" />
